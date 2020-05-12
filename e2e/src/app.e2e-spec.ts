8fa61aee468837db10b43a58f3b0b9a90a551884
Kate Sky <kateskytech@outlook.com>
2020-05-12 15:25:00 -0500

    fixed names

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngHouston-demo app is running!');
+    expect(page.getTitleText()).toEqual('angular-air-demo app is running!');
   });
 
   afterEach(async () => {
